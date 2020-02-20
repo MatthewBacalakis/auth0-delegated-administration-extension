79dd26f867c298923d3ec51d1cf2f77b8e111c25
Matt Bacalakis <matt.bacalakis@gmail.com>
2020-02-20 16:08:09 -0500

    Revert "html file tweak"
    This reverts commit 7b563ce28d2b9fa6de6aa235ecac1a094cd9697c.

--- server/routes/html.js
+++ server/routes/html.js
@@ -1,10 +1,10 @@
-import fs from "fs";
-import url from "url";
-import ejs from "ejs";
-import path from "path";
-import { urlHelpers } from "auth0-extension-express-tools";
+import fs from 'fs';
+import url from 'url';
+import ejs from 'ejs';
+import path from 'path';
+import { urlHelpers } from 'auth0-extension-express-tools';
 
-import config from "../lib/config";
+import config from '../lib/config';
 
 export default () => {
   const template = `
@@ -21,7 +21,7 @@ export default () => {
       <link rel="stylesheet" type="text/css" href="https://cdn.auth0.com/manage/v0.3.1672/css/index.min.css" />
       <link rel="stylesheet" type="text/css" href="https://cdn.auth0.com/styleguide/4.6.13/index.min.css" />
       <% if (assets.style) { %><link rel="stylesheet" type="text/css" href="/app/<%= assets.style %>" /><% } %>
-      <% if (assets.version) { %><link rel="stylesheet" type="text/css" href="<%= assets.cdnPath %>/auth0-delegated-admin-custom.ui.<%= assets.version %>.css" /><% } %>
+      <% if (assets.version) { %><link rel="stylesheet" type="text/css" href="<%= assets.cdnPath %>/auth0-delegated-admin.ui.<%= assets.version %>.css" /><% } %>
       <% if (assets.customCss) { %><link rel="stylesheet" type="text/css" href="<%= assets.customCss %>" /><% } %>
     </head>
     <body>
@@ -32,52 +32,57 @@ export default () => {
       <% if (assets.vendors) { %><script type="text/javascript" src="/app/<%= assets.vendors %>"></script><% } %>
       <% if (assets.app) { %><script type="text/javascript" src="/app/<%= assets.app %>"></script><% } %>
       <% if (assets.version) { %>
-      <script type="text/javascript" src="<%= assets.cdnPath %>/auth0-delegated-admin-custom.ui.vendors.<%= assets.version %>.js"></script>
-      <script type="text/javascript" src="<%= assets.cdnPath %>/auth0-delegated-admin-custom.ui.<%= assets.version %>.js"></script>
+      <script type="text/javascript" src="<%= assets.cdnPath %>/auth0-delegated-admin.ui.vendors.<%= assets.version %>.js"></script>
+      <script type="text/javascript" src="<%= assets.cdnPath %>/auth0-delegated-admin.ui.<%= assets.version %>.js"></script>
       <% } %>
     </body>
     </html>
     `;
 
-  const getLocale = req => {
+  const getLocale = (req) => {
     const basePath = urlHelpers.getBasePath(req);
     const pathname = url.parse(req.originalUrl).pathname;
-    const relativePath = pathname.replace(basePath, "").split("/");
-    const routes = ["api", "login", "logs", "configuration", "users"];
-    if (routes.indexOf(relativePath[0]) < 0 && relativePath[0] !== "") {
+    const relativePath = pathname.replace(basePath, '').split('/');
+    const routes = [
+      'api',
+      'login',
+      'logs',
+      'configuration',
+      'users'
+    ];
+    if (routes.indexOf(relativePath[0]) < 0 && relativePath[0] !== '') {
       return relativePath[0];
     }
 
-    return req.cookies["dae-locale"] || "en";
+    return req.cookies['dae-locale'] || 'en';
   };
 
   return (req, res, next) => {
-    if (req.url.indexOf("/api") === 0) {
+    if (req.url.indexOf('/api') === 0) {
       return next();
     }
 
     const locale = getLocale(req);
     const basePath = urlHelpers.getBasePath(req);
 
-    if (req.url.indexOf("/login") !== 0) {
-      res.cookie("dae-locale", locale);
+    if (req.url.indexOf('/login') !== 0) {
+      res.cookie('dae-locale', locale);
       if (req.url.indexOf(`/${locale}`) !== 0) {
-        return res.redirect(`${basePath}${locale}${req.url || "/login"}`);
+        return res.redirect(`${basePath}${locale}${req.url || '/login'}`);
       }
     }
 
     const settings = {
-      AUTH0_DOMAIN: config("AUTH0_DOMAIN"),
-      AUTH0_CUSTOM_DOMAIN: config("AUTH0_CUSTOM_DOMAIN"),
-      IS_APPLIANCE: config("IS_APPLIANCE"),
-      AUTH0_CLIENT_ID: config("EXTENSION_CLIENT_ID"),
-      EXTEND_URL: config("EXTEND_URL"),
+      AUTH0_DOMAIN: config('AUTH0_DOMAIN'),
+      AUTH0_CUSTOM_DOMAIN: config('AUTH0_CUSTOM_DOMAIN'),
+      IS_APPLIANCE: config('IS_APPLIANCE'),
+      AUTH0_CLIENT_ID: config('EXTENSION_CLIENT_ID'),
+      EXTEND_URL: config('EXTEND_URL'),
       BASE_URL: urlHelpers.getBaseUrl(req),
       BASE_PATH: `${basePath}${locale}/`,
-      TITLE: config("TITLE"),
-      FEDERATED_LOGOUT: config("FEDERATED_LOGOUT") === "true",
-      AUTH0_MANAGE_URL:
-        config("AUTH0_MANAGE_URL") || "https://manage.auth0.com",
+      TITLE: config('TITLE'),
+      FEDERATED_LOGOUT: config('FEDERATED_LOGOUT') === 'true',
+      AUTH0_MANAGE_URL: config('AUTH0_MANAGE_URL') || 'https://manage.auth0.com',
       LOCALE: locale
     };
 
@@ -85,51 +90,43 @@ export default () => {
     const clientVersion = process.env.CLIENT_VERSION;
     const PR_NUMBER = process.env.PR_NUMBER;
     if (clientVersion) {
-      const favIcon =
-        config("FAVICON_PATH") ||
-        "https://cdn.auth0.com/styleguide/4.6.13/lib/logos/img/favicon.png";
-      const cdnPath =
-        config("CDN_PATH") ||
-        (PR_NUMBER
+      const favIcon = config('FAVICON_PATH') || 'https://cdn.auth0.com/styleguide/4.6.13/lib/logos/img/favicon.png';
+      const cdnPath = config('CDN_PATH') || (
+        PR_NUMBER
           ? `//s3.amazonaws.com/extensions-review/auth0-delegated-admin-pr-${PR_NUMBER}/assets`
-          : "//cdn.auth0.com/extensions/auth0-delegated-admin/assets");
-
-      return res.send(
-        ejs.render(template, {
-          config: settings,
-          assets: {
-            customCss: config("CUSTOM_CSS"),
-            version: clientVersion,
-            cdnPath: cdnPath,
-            favIcon: favIcon
-          }
-        })
+          : '//cdn.auth0.com/extensions/auth0-delegated-admin/assets'
       );
+
+      return res.send(ejs.render(template, {
+        config: settings,
+        assets: {
+          customCss: config('CUSTOM_CSS'),
+          version: clientVersion,
+          cdnPath: cdnPath,
+          favIcon: favIcon
+        }
+      }));
     }
 
     // Render locally.
-    return fs.readFile(
-      path.join(__dirname, "../../dist/manifest.json"),
-      "utf8",
-      (err, manifest) => {
-        const locals = {
-          config: settings,
-          assets: {
-            customCss: config("CUSTOM_CSS"),
-            app: "bundle.js"
-          }
-        };
-
-        if (!err && manifest) {
-          locals.assets = {
-            customCss: config("CUSTOM_CSS"),
-            ...JSON.parse(manifest)
-          };
+    return fs.readFile(path.join(__dirname, '../../dist/manifest.json'), 'utf8', (err, manifest) => {
+      const locals = {
+        config: settings,
+        assets: {
+          customCss: config('CUSTOM_CSS'),
+          app: 'bundle.js'
         }
+      };
 
-        // Render the HTML page.
-        res.send(ejs.render(template, locals));
+      if (!err && manifest) {
+        locals.assets = {
+          customCss: config('CUSTOM_CSS'),
+          ...JSON.parse(manifest)
+        };
       }
-    );
+
+      // Render the HTML page.
+      res.send(ejs.render(template, locals));
+    });
   };
 };
